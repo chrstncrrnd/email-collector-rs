3a6a99738780f530b56008cf2cefb95036d3a0ae
ChrstnC <chrstncrrnd@gmail.com>
2023-08-31 14:10:25 +0200

    added email lookup back

--- src/routes.rs
+++ src/routes.rs
@@ -1,6 +1,6 @@
 use actix_web::{get, web, HttpResponse, Responder};
 use log::{error, info};
-use mongodb::Client;
+use mongodb::{bson::doc, Client};
 
 use crate::{models::SubmittedEmailModel, validator::email_validator, DB_NAME};
 
@@ -23,6 +23,30 @@ async fn add_email(email: web::Path<String>, client: web::Data<Client>) -> impl 
     let collection = client
         .database(DB_NAME.as_str())
         .collection::<SubmittedEmailModel>("emails");
+
+    // Check if the email exists already in the database
+    match collection
+        .find_one(
+            doc! {
+                "email": email.to_string()
+            },
+            None,
+        )
+        .await
+    {
+        Ok(d) => {
+            if let Some(_) = d {
+                info!("Email {} already in database", email);
+                return HttpResponse::Ok()
+                    .body("No changes were made, email already exists in database");
+            }
+        }
+        Err(e) => {
+            error!("Mongo query failed: {}", e);
+            return HttpResponse::InternalServerError()
+                .body("Something went wrong with database query");
+        }
+    }
 
     // Add the email to the database
     match collection
